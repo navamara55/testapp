300985180005f0856e1deecee9ddf2b40acc7674
navamara <navamara55@gmail.com>
2020-01-04 20:56:37 -0800

    added singlespa

---
+++ config/single-spa.config.js
@@ -0,0 +1,7 @@
+import * as singleSpa from "single-spa";
+const {registerApplication, start} = singleSpa;
+
+
+registerApplication('test', () => import('../src/main.app'),
+() => true);
+start();
---
+++ src/main.app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
+import RootApp from './root.component.js';
+function domElementGetter() {
+  return document.getElementById("root")
+}
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: RootApp,
+  domElementGetter,
+})
+export const bootstrap = [
+  reactLifecycles.bootstrap,
+];
+export const mount = [
+  reactLifecycles.mount,
+];
+export const unmount = [
+  reactLifecycles.unmount,
+];
